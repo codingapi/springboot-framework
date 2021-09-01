62d668f21c82d8dc6dd9e7680870fa041b238061
王亮 <wangliang@codingapi.com>
2021-09-01 09:32:28 +0800

    test

--- springboot-framework/src/test/java/com/codingapi/springboot/framework/FrameworkApplicationTests.java
+++ springboot-framework/src/test/java/com/codingapi/springboot/framework/FrameworkApplicationTests.java
@@ -1,9 +1,8 @@
 package com.codingapi.springboot.framework;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FrameworkApplicationTests {
 
 	@Test
