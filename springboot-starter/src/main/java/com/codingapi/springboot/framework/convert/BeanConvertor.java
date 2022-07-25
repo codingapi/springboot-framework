5b2a013abe635c2dfbeaedd9c34234143ff0da24
1991wangliang@gmail.com <1991wangliang@gmail.com>
2022-07-25 10:39:09 +0800

    add BeanConvertor

---
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/convert/BeanConvertor.java
@@ -0,0 +1,23 @@
+package com.codingapi.springboot.framework.convert;
+
+import org.springframework.beans.BeanUtils;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class BeanConvertor {
+
+    public static <T,S> T convert(S source, Class<T> clazz){
+        if(source==null){
+            return null;
+        }
+        T target = null;
+        try {
+            target =  clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            return null;
+        }
+        BeanUtils.copyProperties(source,target);
+        return target;
+    }
+
+}
