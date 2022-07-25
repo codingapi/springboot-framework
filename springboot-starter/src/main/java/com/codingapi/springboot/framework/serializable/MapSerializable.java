3980d45ca0af1677c1b1c76c43af8d6852697c49
王亮 <wangliang@codingapi.com>
2022-07-25 10:10:35 +0800

    add MapSerializable

---
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/serializable/MapSerializable.java
@@ -0,0 +1,16 @@
+package com.codingapi.springboot.framework.serializable;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Map;
+
+/**
+ * @author lorne
+ * @since 1.0.0
+ */
+public interface MapSerializable {
+
+    default Map<String,Object> toMap(){
+        return (Map<String,Object>) JSONObject.toJSON(this);
+    }
+}
