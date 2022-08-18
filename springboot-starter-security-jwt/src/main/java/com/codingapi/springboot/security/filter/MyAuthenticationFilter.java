bcfd5219626d5a0a35369431d6eafdff347a6392
1991wangliang@gmail.com <1991wangliang@gmail.com>
2022-08-18 10:57:46 +0800

    fix token reset bug

--- springboot-starter-security-jwt/src/main/java/com/codingapi/springboot/security/filter/MyAuthenticationFilter.java
+++ springboot-starter-security-jwt/src/main/java/com/codingapi/springboot/security/filter/MyAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class MyAuthenticationFilter extends BasicAuthenticationFilter {
 
         Token token = jwt.parser(sign);
         if(token.canRestToken()){
-            Token newSign = jwt.create(token.getUsername(),token.getCertificate(),token.getAuthorities());
+            Token newSign = jwt.create(token.getUsername(),token.getDecodePassword(),token.getAuthorities());
             log.info("reset token ");
             response.setHeader(TOKEN_KEY,newSign.getToken());
         }
