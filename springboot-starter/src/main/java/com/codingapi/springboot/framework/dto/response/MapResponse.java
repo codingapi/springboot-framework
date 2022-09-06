ceb4acc8e4ed063ed11652dd8e45d81942b0f46d
1991wangliang <1991wangliang@gmail.com>
2022-09-06 13:45:14 +0800

    add MapResponse

---
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/dto/response/MapResponse.java
@@ -0,0 +1,36 @@
+package com.codingapi.springboot.framework.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Getter
+public class MapResponse extends Response{
+
+    private Map<String,Object> data;
+
+    public MapResponse() {
+        this.data = new HashMap<>();
+    }
+
+    public static MapResponse create() {
+        MapResponse singleResponse = new MapResponse();
+        singleResponse.setSuccess(true);
+        return singleResponse;
+    }
+
+    public static MapResponse empty() {
+        MapResponse mapResponse = new MapResponse();
+        mapResponse.setSuccess(true);
+        mapResponse.setData(null);
+        return mapResponse;
+    }
+
+    public MapResponse add(String key, Object value){
+        this.data.put(key, value);
+        return this;
+    }
+}
