86e1cac3e31ca2e0b1e826908e5c25245cf4b1de
1991wangliang <1991wangliang@gmail.com>
2022-09-11 20:17:39 +0800

    add junit test to starter

---
+++ springboot-starter/src/test/java/com/codingapi/springboot/framework/FrameWorkApplication.java
@@ -0,0 +1,12 @@
+package com.codingapi.springboot.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameWorkApplication.class,args);
+    }
+}
--- springboot-starter/src/test/java/com/codingapi/springboot/framework/FrameworkApplicationTests.java
+++ springboot-starter/src/test/java/com/codingapi/springboot/framework/FrameworkApplicationTests.java
@@ -1,20 +1,14 @@
 package com.codingapi.springboot.framework;
 
-import com.codingapi.springboot.framework.domain.Demo;
-import com.codingapi.springboot.framework.event.SpringEventConfiguration;
-import com.codingapi.springboot.framework.handler.DemoConfiguration;
-import com.codingapi.springboot.framework.handler.HandlerBeanDefinitionRegistrar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {SpringEventConfiguration.class, HandlerBeanDefinitionRegistrar.class, DemoConfiguration.class})
+@SpringBootTest
 class FrameworkApplicationTests {
 
 
     @Test
     void contextLoads() {
-        Demo demo = new Demo("123");
-        demo.changeName("234");
     }
 
 }
