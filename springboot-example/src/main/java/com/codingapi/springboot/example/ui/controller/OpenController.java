79c458160fb630d8ee22be86afbfd152b6b74ae7
1991wangliang <1991wangliang@gmail.com>
2022-09-11 14:25:35 +0800

    remove id-generator test code

--- springboot-example/src/main/java/com/codingapi/springboot/example/ui/controller/OpenController.java
+++ springboot-example/src/main/java/com/codingapi/springboot/example/ui/controller/OpenController.java
@@ -6,16 +6,11 @@ import com.codingapi.springboot.example.infrastructure.jap.repository.DemoEntity
 import com.codingapi.springboot.framework.dto.request.PageRequest;
 import com.codingapi.springboot.framework.dto.response.MultiResponse;
 import com.codingapi.springboot.framework.dto.response.Response;
-import com.codingapi.springboot.generator.dao.IdKeyDao;
-import com.codingapi.springboot.generator.domain.IdKey;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.SQLException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/open/demo")
@@ -25,8 +20,6 @@ public class OpenController {
     private final DemoExecutor executor;
 
     private final DemoEntityRepository demoEntityRepository;
-
-    private final IdKeyDao idKeyDao;
 
     @GetMapping("/save")
     public Response save(@RequestParam("name") String name) {
@@ -38,18 +31,4 @@ public class OpenController {
     public MultiResponse<DemoEntity> findAll(PageRequest pageRequest) {
         return MultiResponse.of(demoEntityRepository.findAll(pageRequest));
     }
-
-
-    @GetMapping("/test-list")
-    public List<IdKey> test1() throws SQLException {
-        return idKeyDao.findAll();
-    }
-
-    @GetMapping("/test-save")
-    public Response test2() throws SQLException {
-        IdKey generator = new IdKey("xxx");
-        idKeyDao.save(generator);
-        return Response.buildSuccess();
-    }
-
 }
