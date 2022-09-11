9a1f179f556fea860916bd5a3128f6c45692957b
1991wangliang <1991wangliang@gmail.com>
2022-09-11 20:13:55 +0800

    add junit test to security-jwt

---
+++ springboot-starter-security-jwt/src/test/java/com/codingapi/springboot/security/SecurityJwtApplication.java
@@ -0,0 +1,12 @@
+package com.codingapi.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtApplication.class,args);
+    }
+}
---
+++ springboot-starter-security-jwt/src/test/java/com/codingapi/springboot/security/SecurityJwtApplicationTest.java
@@ -0,0 +1,15 @@
+package com.codingapi.springboot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SecurityJwtApplicationTest {
+
+
+    @Test
+    void test(){
+
+    }
+
+}
