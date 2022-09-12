ad7ceed34667bc4e61abad2b751942bfaab878ff
1991wangliang <1991wangliang@gmail.com>
2022-09-12 22:07:21 +0800

    add junit test

--- springboot-starter-security-jwt/src/test/java/com/codingapi/springboot/security/SecurityJwtApplicationTest.java
+++ springboot-starter-security-jwt/src/test/java/com/codingapi/springboot/security/SecurityJwtApplicationTest.java
@@ -9,13 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.StringUtils;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.junit.jupiter.api.Assertions.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -55,6 +57,45 @@ public class SecurityJwtApplicationTest {
         mockMvc.perform(get("/api/hello").header("Authorization",loginData.getJSONObject("data").getString("token")).contentType(MediaType.APPLICATION_JSON)).andExpect(result -> {
             String body = result.getResponse().getContentAsString();
             assertEquals(body,"hello","token authentication error");
+        });
+    }
+
+
+    @Test
+    void resetToken() throws Exception {
+
+        JSONObject json = new JSONObject();
+        json.put("username","admin");
+        json.put("password","123456");
+        MvcResult mvcResult =  mockMvc.perform(post("/user/login").content(json.toJSONString().getBytes(StandardCharsets.UTF_8)).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        JSONObject loginData = JSONObject.parseObject(mvcResult.getResponse().getContentAsString());
+
+        Thread.sleep(1000*5);
+
+        mockMvc.perform(get("/api/hello").header("Authorization",loginData.getJSONObject("data").getString("token")).contentType(MediaType.APPLICATION_JSON)).andExpect(result -> {
+            String body = result.getResponse().getContentAsString();
+            String newToken = result.getResponse().getHeader("Authorization");
+
+            assertTrue(StringUtils.hasText(newToken),"token reset error");
+            assertEquals(body,"hello","token authentication error");
+        });
+    }
+
+    @Test
+    void expireToken() throws Exception {
+
+        JSONObject json = new JSONObject();
+        json.put("username","admin");
+        json.put("password","123456");
+        MvcResult mvcResult =  mockMvc.perform(post("/user/login").content(json.toJSONString().getBytes(StandardCharsets.UTF_8)).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        JSONObject loginData = JSONObject.parseObject(mvcResult.getResponse().getContentAsString());
+
+        Thread.sleep(1000*10);
+
+        mockMvc.perform(get("/api/hello").header("Authorization",loginData.getJSONObject("data").getString("token")).contentType(MediaType.APPLICATION_JSON)).andExpect(result -> {
+            String body = result.getResponse().getContentAsString();
+            JSONObject data = JSONObject.parseObject(body);
+            assertEquals(data.getString("errCode"),"token.expire","token authentication error");
         });
     }
 
