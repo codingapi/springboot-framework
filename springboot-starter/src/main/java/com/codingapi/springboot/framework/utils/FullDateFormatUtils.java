7dbd6aa5fb24fc6e9a6cca0a5766adaf89e235e3
1991wangliang <1991wangliang@gmail.com>
2022-09-19 21:03:23 +0800

    fix FullDateFormatUtils bug

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/utils/FullDateFormatUtils.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/utils/FullDateFormatUtils.java
@@ -3,9 +3,14 @@ package com.codingapi.springboot.framework.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class FullDateFormatUtils {
     private final static SimpleDateFormat defaultFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    static {
+        defaultFormat.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+    }
 
     public static String convert(long timestamp){
         return convert(new Date(timestamp));
