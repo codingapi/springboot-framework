a19c841c23df8cad515727cf115a89ac73af3737
1991wangliang <1991wangliang@gmail.com>
2022-09-26 22:01:10 +0800

    remove example

--- springboot-example/src/main/java/com/codingapi/springboot/example/infrastructure/jap/repository/DemoEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codingapi.springboot.example.infrastructure.jap.repository;
-
-import com.codingapi.springboot.example.infrastructure.entity.DemoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DemoEntityRepository extends JpaRepository<DemoEntity, Integer> {
-
-
-}
