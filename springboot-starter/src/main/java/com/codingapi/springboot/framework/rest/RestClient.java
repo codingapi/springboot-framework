c66b8909d448090c8562419c584dec30b05b0358
lorne <1991wangliang@gmail.com>
2023-02-10 21:28:52 +0800

    add 1.4.13.dev
    1. add rest header

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/rest/RestClient.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/rest/RestClient.java
@@ -5,6 +5,7 @@ import com.codingapi.springboot.framework.rest.param.ApiGetParamBuilder;
 import com.codingapi.springboot.framework.rest.param.ApiPostParamBuilder;
 import com.codingapi.springboot.framework.rest.properties.RestApiProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 
 @Slf4j
 public class RestClient {
@@ -24,39 +25,56 @@ public class RestClient {
     }
 
     private String _get(String api, ApiGetParamBuilder paramBuilder) {
-        return httpClient.get(api, paramBuilder!=null?paramBuilder.build():null);
+        return _get(api,new HttpHeaders(),paramBuilder);
     }
 
-    public String get(String api, ApiGetParamBuilder paramBuilder) {
+    private String _get(String api,HttpHeaders headers, ApiGetParamBuilder paramBuilder) {
+        return httpClient.get(api, headers,paramBuilder!=null?paramBuilder.build():null);
+    }
+
+    public String get(String api,HttpHeaders headers, ApiGetParamBuilder paramBuilder) {
         for(int i=0; i< RETRY_COUNT; i++){
             try {
-                return _get(api, paramBuilder);
+                return _get(api,headers, paramBuilder);
             }catch (Exception e){
                 log.warn("api:{},error:{}",api,e.getMessage());
                 sleep();
             }
         }
         return EMPTY;
+    }
+    public String get(String api, ApiGetParamBuilder paramBuilder) {
+        return get(api,new HttpHeaders(),paramBuilder);
     }
 
     public String get(String api) {
-        return get(api,null);
+        return get(api,new HttpHeaders(),null);
     }
 
-    private String _post(String api, ApiPostParamBuilder paramBuilder) {
-        return _post(api, paramBuilder.build());
+    public String get(String api,HttpHeaders headers) {
+        return get(api,headers,null);
     }
-
 
     private String _post(String api, JSONObject requestBody) {
-        return httpClient.post(api, requestBody);
+        return _post(api,new HttpHeaders(),requestBody);
     }
 
+    private String _post(String api, HttpHeaders headers, JSONObject requestBody) {
+        return httpClient.post(api,headers, requestBody);
+    }
 
     public String post(String api, JSONObject requestBody) {
+        return post(api,new HttpHeaders(),requestBody);
+    }
+
+    public String post(String api, ApiPostParamBuilder paramBuilder) {
+        return post(api,new HttpHeaders(),paramBuilder.build());
+    }
+
+    public String post(String api,HttpHeaders headers, JSONObject requestBody) {
         for (int i=0;i< RETRY_COUNT;i++){
             try {
-                return _post(api, requestBody);
+                return _post(api, headers, requestBody);
             }catch (Exception e){
                 log.warn("api:{},error:{}",api,e.getMessage());
                 sleep();
@@ -65,17 +83,8 @@ public class RestClient {
         return EMPTY;
     }
 
-
-    public String post(String api, ApiPostParamBuilder paramBuilder) {
-        for (int i=0;i< RETRY_COUNT;i++){
-            try {
-                return _post(api, paramBuilder);
-            }catch (Exception e){
-                log.warn("api:{},error:{}",api,e.getMessage());
-                sleep();
-            }
-        }
-        return EMPTY;
+    public String post(String api,HttpHeaders headers, ApiPostParamBuilder paramBuilder) {
+       return post(api, headers, paramBuilder.build());
     }
 
 
