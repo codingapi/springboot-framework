9927777877333eda841f49b921b56e0b02b024b5
lorne <1991wangliang@gmail.com>
2023-04-28 11:12:30 +0800

    fix Event & IdGenerator null bug

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/event/SpringEventInitializer.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/event/SpringEventInitializer.java
@@ -1,15 +1,16 @@
 package com.codingapi.springboot.framework.event;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 
 @AllArgsConstructor
-public class SpringEventInitializer {
+public class SpringEventInitializer implements InitializingBean {
 
     private final ApplicationContext context;
 
-    public void init() {
+    @Override
+    public void afterPropertiesSet() throws Exception {
         DomainEventContext.getInstance().initContext(context);
     }
-
 }
