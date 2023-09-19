66b32af294f9663c899e10079c0e9f38807cf808
lorne <1991wangliang@gmail.com>
2023-09-19 17:47:36 +0800

    fix HandlerExceptionResolver @Configuration

---
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/servlet/BasicHandlerExceptionResolverConfiguration.java
@@ -0,0 +1,50 @@
+package com.codingapi.springboot.framework.servlet;
+
+
+import com.codingapi.springboot.framework.exception.LocaleMessageException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configuration
+@ConditionalOnClass(name = "org.springframework.web.servlet.HandlerExceptionResolver")
+public class BasicHandlerExceptionResolverConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public HandlerExceptionResolver servletExceptionHandler() {
+        return new ServletExceptionHandler();
+    }
+
+    @Slf4j
+    public static class ServletExceptionHandler implements HandlerExceptionResolver {
+
+        private final static String DEFAULT_ERROR_CODE = "system.err";
+
+        @Override
+        public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+            MappingJackson2JsonView view = new MappingJackson2JsonView();
+            ModelAndView mv = new ModelAndView(view);
+            mv.addObject("success", false);
+            if (ex instanceof LocaleMessageException) {
+                LocaleMessageException localMessageException = (LocaleMessageException) ex;
+                mv.addObject("errCode", localMessageException.getErrCode());
+                mv.addObject("errMessage", localMessageException.getMessage());
+                log.warn("controller errCode:{} exception:{}", localMessageException.getErrCode(), ex.getLocalizedMessage());
+                return mv;
+            }
+            log.warn("controller exception:{}", ex.getLocalizedMessage(), ex);
+            mv.addObject("errCode", DEFAULT_ERROR_CODE);
+            mv.addObject("errMessage", ex.getMessage());
+
+            return mv;
+        }
+    }
+}
