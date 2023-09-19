c15281564b30af139d28e05682073bdf2bd219b5
xlorne <1991wangliang@gmail.com>
2023-09-19 16:18:08 +0800

    remove tomcat-embed-core

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/AutoConfiguration.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/AutoConfiguration.java
@@ -1,14 +1,19 @@
 package com.codingapi.springboot.framework;
 
 import com.codingapi.springboot.framework.servlet.ServletExceptionHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
 
 @Configuration
 public class AutoConfiguration {
 
     @Bean
+    @ConditionalOnClass(value = {HandlerExceptionResolver.class, ModelAndView.class, MappingJackson2JsonView.class})
     public HandlerExceptionResolver servletExceptionHandler() {
         return new ServletExceptionHandler();
     }
