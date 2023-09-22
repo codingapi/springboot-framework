80e580db3f7b23eb26227565d9f5106b1f7a3bf7
xlorne <1991wangliang@gmail.com>
2023-09-22 12:26:38 +0800

    add findAllByRequest

--- springboot-starter-data-fast/src/main/java/com/codingapi/springboot/fast/query/FastRepository.java
+++ springboot-starter-data-fast/src/main/java/com/codingapi/springboot/fast/query/FastRepository.java
@@ -1,6 +1,7 @@
 package com.codingapi.springboot.fast.query;
 
 import com.codingapi.springboot.framework.dto.request.PageRequest;
+import jakarta.persistence.criteria.Order;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.core.ResolvableType;
 import org.springframework.data.domain.Page;
@@ -9,8 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @NoRepositoryBean
@@ -19,8 +18,8 @@ public interface FastRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecific
     default Page<T> findAll(PageRequest request) {
         if (request.hasFilter()) {
             Class<T> clazz = getDomainClass();
-            QueryRequest queryRequest = new QueryRequest(request);
-            return findAll(queryRequest.getExample(clazz), request);
+            QueryRequest queryRequest = new QueryRequest(request, clazz);
+            return findAll(queryRequest.getExample(), request);
         }
         return findAll((org.springframework.data.domain.PageRequest) request);
     }
@@ -35,98 +34,11 @@ public interface FastRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecific
 
     default Page<T> findAllByRequest(PageRequest request) {
         if (request.hasFilter()) {
+            Class<T> clazz = getDomainClass();
             Specification<T> specification = (root, query, criteriaBuilder) -> {
-
-                List<Predicate> predicates = new ArrayList<>();
-                for (String key : request.getFilters().keySet()) {
-                    PageRequest.Filter filter = request.getFilters().get(key);
-                    if (filter.isEqual()) {
-                        predicates.add(criteriaBuilder.equal(root.get(key), filter.getValue()[0]));
-                    }
-
-                    if (filter.isLike()) {
-                        String matchValue = (String) filter.getValue()[0];
-                        predicates.add(criteriaBuilder.like(root.get(key), "%" + matchValue + "%"));
-                    }
-
-                    if (filter.isBetween()) {
-                        Object value1 = filter.getValue()[0];
-                        Object value2 = filter.getValue()[2];
-                        if (value1 instanceof Integer && value2 instanceof Integer) {
-                            predicates.add(criteriaBuilder.between(root.get(key), (Integer) value1, (Integer) value2));
-                        }
-
-                        if (value1 instanceof Long && value2 instanceof Long) {
-                            predicates.add(criteriaBuilder.between(root.get(key), (Long) value1, (Long) value2));
-                        }
-
-                        if (value1 instanceof Date && value2 instanceof Date) {
-                            predicates.add(criteriaBuilder.between(root.get(key), (Date) value1, (Date) value2));
-                        }
-                    }
-
-                    if (filter.isGreaterThan()) {
-                        Object value = filter.getValue()[0];
-                        if (value instanceof Integer) {
-                            predicates.add(criteriaBuilder.greaterThan(root.get(key), (Integer) value));
-                        }
-                        if (value instanceof Long) {
-                            predicates.add(criteriaBuilder.greaterThan(root.get(key), (Long) value));
-                        }
-                        if (value instanceof Date) {
-                            predicates.add(criteriaBuilder.greaterThan(root.get(key), (Date) value));
-                        }
-                    }
-
-                    if (filter.isGreaterThanEqual()) {
-                        Object value = filter.getValue()[0];
-                        if (value instanceof Integer) {
-                            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), (Integer) value));
-                        }
-                        if (value instanceof Long) {
-                            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), (Long) value));
-                        }
-                        if (value instanceof Date) {
-                            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), (Date) value));
-                        }
-                    }
-
-                    if (filter.isLessThan()) {
-                        Object value = filter.getValue()[0];
-                        if (value instanceof Integer) {
-                            predicates.add(criteriaBuilder.lessThan(root.get(key), (Integer) value));
-                        }
-                        if (value instanceof Long) {
-                            predicates.add(criteriaBuilder.lessThan(root.get(key), (Long) value));
-                        }
-                        if (value instanceof Date) {
-                            predicates.add(criteriaBuilder.lessThan(root.get(key), (Date) value));
-                        }
-                    }
-
-                    if (filter.isLessThanEqual()) {
-                        Object value = filter.getValue()[0];
-                        if (value instanceof Integer) {
-                            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), (Integer) value));
-                        }
-                        if (value instanceof Long) {
-                            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), (Long) value));
-                        }
-                        if (value instanceof Date) {
-                            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), (Date) value));
-                        }
-                    }
-                }
-
-                List<jakarta.persistence.criteria.Order> orderList = new ArrayList<>();
-                request.getSort().forEach(sort -> {
-                    if (sort.getDirection().isAscending()) {
-                        orderList.add(criteriaBuilder.asc(root.get(sort.getProperty())));
-                    } else {
-                        orderList.add(criteriaBuilder.asc(root.get(sort.getProperty())));
-                    }
-                });
-
+                QueryRequest queryRequest = new QueryRequest(request, clazz);
+                List<Predicate> predicates = queryRequest.getPredicate(root, criteriaBuilder);
+                List<Order> orderList = queryRequest.getOrder(root, criteriaBuilder);
                 return query.where(predicates.toArray(new Predicate[0])).orderBy(orderList).getRestriction();
             };
 
--- springboot-starter-data-fast/src/main/java/com/codingapi/springboot/fast/query/QueryRequest.java
+++ springboot-starter-data-fast/src/main/java/com/codingapi/springboot/fast/query/QueryRequest.java
@@ -1,20 +1,29 @@
 package com.codingapi.springboot.fast.query;
 
 import com.codingapi.springboot.framework.dto.request.PageRequest;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Example;
 
 import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class QueryRequest {
 
     private final PageRequest request;
+    private final Class<?> clazz;
 
-    public QueryRequest(PageRequest request) {
+    public QueryRequest(PageRequest request, Class<?> clazz) {
         this.request = request;
+        this.clazz = clazz;
     }
 
-    public <T> Example<T> getExample(Class<T> clazz) {
+    public <T> Example<T> getExample() {
         if (!request.hasFilter()) {
             return null;
         }
@@ -36,5 +45,103 @@ public class QueryRequest {
             }
         }
         return (Example<T>) Example.of(entity);
+    }
+
+
+    public <T> List<Order> getOrder(Root<T> root, CriteriaBuilder criteriaBuilder) {
+        List<Order> orderList = new ArrayList<>();
+        request.getSort().forEach(sort -> {
+            if (sort.getDirection().isAscending()) {
+                orderList.add(criteriaBuilder.asc(root.get(sort.getProperty())));
+            } else {
+                orderList.add(criteriaBuilder.asc(root.get(sort.getProperty())));
+            }
+        });
+        return orderList;
+    }
+
+    public <T> List<Predicate> getPredicate(Root<T> root, CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+        for (String key : request.getFilters().keySet()) {
+            PageRequest.Filter filter = request.getFilters().get(key);
+            if (filter.isEqual()) {
+                predicates.add(criteriaBuilder.equal(root.get(key), filter.getValue()[0]));
+            }
+
+            if (filter.isLike()) {
+                String matchValue = (String) filter.getValue()[0];
+                predicates.add(criteriaBuilder.like(root.get(key), "%" + matchValue + "%"));
+            }
+
+            if (filter.isBetween()) {
+                Object value1 = filter.getValue()[0];
+                Object value2 = filter.getValue()[2];
+                if (value1 instanceof Integer && value2 instanceof Integer) {
+                    predicates.add(criteriaBuilder.between(root.get(key), (Integer) value1, (Integer) value2));
+                }
+
+                if (value1 instanceof Long && value2 instanceof Long) {
+                    predicates.add(criteriaBuilder.between(root.get(key), (Long) value1, (Long) value2));
+                }
+
+                if (value1 instanceof Date && value2 instanceof Date) {
+                    predicates.add(criteriaBuilder.between(root.get(key), (Date) value1, (Date) value2));
+                }
+            }
+
+            if (filter.isGreaterThan()) {
+                Object value = filter.getValue()[0];
+                if (value instanceof Integer) {
+                    predicates.add(criteriaBuilder.greaterThan(root.get(key), (Integer) value));
+                }
+                if (value instanceof Long) {
+                    predicates.add(criteriaBuilder.greaterThan(root.get(key), (Long) value));
+                }
+                if (value instanceof Date) {
+                    predicates.add(criteriaBuilder.greaterThan(root.get(key), (Date) value));
+                }
+            }
+
+            if (filter.isGreaterThanEqual()) {
+                Object value = filter.getValue()[0];
+                if (value instanceof Integer) {
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), (Integer) value));
+                }
+                if (value instanceof Long) {
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), (Long) value));
+                }
+                if (value instanceof Date) {
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), (Date) value));
+                }
+            }
+
+            if (filter.isLessThan()) {
+                Object value = filter.getValue()[0];
+                if (value instanceof Integer) {
+                    predicates.add(criteriaBuilder.lessThan(root.get(key), (Integer) value));
+                }
+                if (value instanceof Long) {
+                    predicates.add(criteriaBuilder.lessThan(root.get(key), (Long) value));
+                }
+                if (value instanceof Date) {
+                    predicates.add(criteriaBuilder.lessThan(root.get(key), (Date) value));
+                }
+            }
+
+            if (filter.isLessThanEqual()) {
+                Object value = filter.getValue()[0];
+                if (value instanceof Integer) {
+                    predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), (Integer) value));
+                }
+                if (value instanceof Long) {
+                    predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), (Long) value));
+                }
+                if (value instanceof Date) {
+                    predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), (Date) value));
+                }
+            }
+        }
+
+        return predicates;
     }
 }
