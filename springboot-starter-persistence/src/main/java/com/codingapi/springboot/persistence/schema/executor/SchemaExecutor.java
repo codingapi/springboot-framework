385c944dcbf0499da7b249e77148ff22638f25d5
lorne <1991wangliang@gmail.com>
2024-01-01 22:52:53 +0800

    remove

--- springboot-starter-persistence/src/main/java/com/codingapi/springboot/persistence/schema/executor/SchemaExecutor.java
+++
@@ -1,9 +0,0 @@
-package com.codingapi.springboot.persistence.schema.executor;
-
-import com.codingapi.springboot.persistence.schema.Schema;
-
-public interface SchemaExecutor {
-
-    void create(Schema schema);
-
-}
--- springboot-starter-persistence/src/main/java/com/codingapi/springboot/persistence/schema/factory/SchemaFactory.java
+++
@@ -1,8 +0,0 @@
-package com.codingapi.springboot.persistence.schema.factory;
-
-import com.codingapi.springboot.persistence.schema.Schema;
-
-public interface SchemaFactory {
-
-    Schema getSchema(Class<?> domainClass);
-}
