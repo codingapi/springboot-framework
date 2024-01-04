917661c6d91c6330f66d820c7746118d725c516a
xlorne <1991wangliang@gmail.com>
2024-01-04 23:32:01 +0800

    fix  FastRepository#pageRequest

---
+++ springboot-starter-data-fast/src/test/java/com/codingapi/springboot/fast/entity/Menu.java
@@ -0,0 +1,22 @@
+package com.codingapi.springboot.fast.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@Entity
+@ToString
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @ManyToOne
+    private Menu parent;
+
+}
