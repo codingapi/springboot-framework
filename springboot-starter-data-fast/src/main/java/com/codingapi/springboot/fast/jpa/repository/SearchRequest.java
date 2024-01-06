aafe75586a7097589536ba06ca988d84d1c213c4
xlorne <1991wangliang@gmail.com>
2024-01-06 18:14:41 +0800

    add SearchRequest

--- springboot-starter-data-fast/src/main/java/com/codingapi/springboot/fast/jpa/repository/SearchRequest.java
+++ springboot-starter-data-fast/src/main/java/com/codingapi/springboot/fast/jpa/repository/SearchRequest.java
@@ -3,6 +3,7 @@ package com.codingapi.springboot.fast.jpa.repository;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.codingapi.springboot.framework.dto.request.Filter;
 import com.codingapi.springboot.framework.dto.request.PageRequest;
 import com.codingapi.springboot.framework.dto.request.Relation;
 import javax.servlet.http.HttpServletRequest;
@@ -52,6 +53,30 @@ public class SearchRequest {
         return new String(Base64.getDecoder().decode(value));
     }
 
+
+    public void addSort(Sort sort) {
+        pageRequest.addSort(sort);
+    }
+
+    public void removeFilter(String key) {
+        pageRequest.removeFilter(key);
+    }
+
+    public PageRequest addFilter(String key, Relation relation, Object... value) {
+        return pageRequest.addFilter(key, relation, value);
+    }
+
+    public PageRequest addFilter(String key, Object... value) {
+        return pageRequest.addFilter(key, value);
+    }
+
+    public PageRequest andFilter(Filter... filters) {
+        return pageRequest.andFilter(filters);
+    }
+
+    public PageRequest orFilters(Filter... filters) {
+        return pageRequest.orFilters(filters);
+    }
 
     static class ClassContent {
 
