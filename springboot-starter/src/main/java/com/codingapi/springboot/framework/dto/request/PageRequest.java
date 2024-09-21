03facd2d2148d6f324ff25fd46335ef988b37fd5
lorne <1991wangliang@gmail.com>
2024-09-21 18:38:28 +0800

    #50

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/dto/request/PageRequest.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/dto/request/PageRequest.java
@@ -2,10 +2,7 @@ package com.codingapi.springboot.framework.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-
-import java.util.Optional;
 
 public class PageRequest extends org.springframework.data.domain.PageRequest {
 
@@ -14,20 +11,20 @@ public class PageRequest extends org.springframework.data.domain.PageRequest {
     private int current;
 
     @Setter
-    @Getter
     private int pageSize;
+
+    @Getter
+    private Sort sort;
 
     @Getter
     private final RequestFilter requestFilter = new RequestFilter();
 
 
-    private org.springframework.data.domain.PageRequest pageRequest;
-
     public PageRequest(int current, int pageSize, Sort sort) {
         super(current, pageSize, sort);
         this.current = current;
         this.pageSize = pageSize;
-        this.pageRequest = org.springframework.data.domain.PageRequest.of(current, pageSize, sort);
+        this.sort = sort;
     }
 
 
@@ -39,6 +36,7 @@ public class PageRequest extends org.springframework.data.domain.PageRequest {
         return requestFilter.getStringFilter(key);
     }
 
+
     public String getStringFilter(String key, String defaultValue) {
         return requestFilter.getStringFilter(key, defaultValue);
     }
@@ -47,9 +45,11 @@ public class PageRequest extends org.springframework.data.domain.PageRequest {
         return requestFilter.getIntFilter(key);
     }
 
+
     public int getIntFilter(String key, int defaultValue) {
         return requestFilter.getIntFilter(key, defaultValue);
     }
+
 
     public boolean hasFilter() {
         return requestFilter.hasFilter();
@@ -57,23 +57,8 @@ public class PageRequest extends org.springframework.data.domain.PageRequest {
 
 
     @Override
-    public Sort getSort() {
-        return pageRequest.getSort();
-    }
-
-    @Override
-    public PageRequest next() {
-        return new PageRequest(current + 1, getPageSize(), getSort());
-    }
-
-    @Override
-    public PageRequest previous() {
-        return current == 0 ? this : new PageRequest(current - 1, getPageSize(), getSort());
-    }
-
-    @Override
-    public PageRequest first() {
-        return new PageRequest(0, getPageSize(), getSort());
+    public int getPageSize() {
+        return pageSize;
     }
 
     @Override
@@ -91,37 +76,12 @@ public class PageRequest extends org.springframework.data.domain.PageRequest {
         return current > 0;
     }
 
-    @Override
-    public Pageable previousOrFirst() {
-        return pageRequest.previousOrFirst();
-    }
-
-    @Override
-    public boolean isPaged() {
-        return pageRequest.isPaged();
-    }
-
-    @Override
-    public boolean isUnpaged() {
-        return pageRequest.isUnpaged();
-    }
-
-    @Override
-    public Sort getSortOr(Sort sort) {
-        return pageRequest.getSortOr(sort);
-    }
-
-    @Override
-    public Optional<Pageable> toOptional() {
-        return pageRequest.toOptional();
-    }
-
     public void addSort(Sort sort) {
-        Sort nowSort = pageRequest.getSort();
+        Sort nowSort = this.sort;
         if (nowSort == Sort.unsorted()) {
-            this.pageRequest = new PageRequest(getCurrent(), getPageSize(), sort);
+            this.sort = sort;
         } else {
-            pageRequest.getSort().and(sort);
+            this.sort.and(sort);
         }
     }
 
