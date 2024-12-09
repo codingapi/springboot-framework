cfa4db2538a230df8d5c69baa8a4972b79cf224a
lorne <1991wangliang@gmail.com>
2024-12-09 23:34:10 +0800

    add <React.StrictMode>

--- admin-ui/src/index.tsx
+++ admin-ui/src/index.tsx
@@ -14,6 +14,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
+    <React.StrictMode>
         <Provider store={store}>
 
             <ConfigProvider
@@ -25,6 +26,7 @@ root.render(
                 <RoutesProvider/>
             </ConfigProvider>
         </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
