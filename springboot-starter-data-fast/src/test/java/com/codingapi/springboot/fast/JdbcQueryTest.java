bd1cc8d7e530a225c79f0e4717d55615c7175fef
lorne <1991wangliang@gmail.com>
2025-09-01 15:52:45 +0800

    fix #144

---
+++ springboot-starter-data-fast/src/test/java/com/codingapi/springboot/fast/JdbcQueryTest.java
@@ -0,0 +1,44 @@
+package com.codingapi.springboot.fast;
+
+import com.codingapi.springboot.fast.entity.Demo;
+import com.codingapi.springboot.fast.repository.DemoRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class JdbcQueryTest {
+
+    @Autowired
+    private DemoRepository demoRepository;
+
+
+    @Test
+    void test() {
+        demoRepository.deleteAll();
+        Demo demo = new Demo();
+        demo.setName("123");
+        demoRepository.save(demo);
+        assertTrue(demo.getId() > 0);
+
+        String sql = "select d.* from t_demo as d where d.id in (d.id,?,?)";
+        String countSql = "select count(d.id) from t_demo as d where d.id in (d.id,?,?)";
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Integer> params = Arrays.asList(demo.getId(), demo.getId());
+        Page<Map<String, Object>> page = demoRepository.dynamicNativeMapPageMapQuery(
+                sql, countSql, pageRequest, params.toArray()
+        );
+        assertEquals(page.getTotalElements(), page.getContent().size());
+    }
+
+}
