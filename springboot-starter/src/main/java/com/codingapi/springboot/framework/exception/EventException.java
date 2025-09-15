1f7f28560cc62a6d0aa7e3c4592661d172fa2cfe
lijianquan <jaquanli@qq.com>
2025-09-15 17:09:00 +0800

    fix(exception):  修复EventException异常链问题 事件异常未能正确挂载suppressed close #153

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/exception/EventException.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/exception/EventException.java
@@ -8,15 +8,15 @@ import java.util.stream.Collectors;
 @Getter
 public class EventException extends RuntimeException {
 
-    private final List<Exception> errors;
+    private final List<Exception> error;
 
-    public EventException(List<Exception> errors) {
-        super(buildMessage(errors), errors.isEmpty() ? null : errors.get(0)); // 第一个异常作为 cause
-        this.errors = errors;
+    public EventException(List<Exception> error) {
+        super(buildMessage(error), error.isEmpty() ? null : error.get(0)); // 第一个异常作为 cause
+        this.error = error;
 
         // 把其他异常挂在 suppressed 上
-        for (int i = 1; i < errors.size(); i++) {
-            this.addSuppressed(errors.get(i));
+        for (int i = 1; i < error.size(); i++) {
+            this.addSuppressed(error.get(i));
         }
     }
     private static String buildMessage(List<Exception> errors) {
