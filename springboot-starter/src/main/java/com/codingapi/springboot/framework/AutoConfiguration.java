4cd0f32ffc40fe5c197e66335d3d64d4640029b0
lorne <1991wangliang@gmail.com>
2025-11-27 22:31:23 +0800

    fix #160

--- springboot-starter/src/main/java/com/codingapi/springboot/framework/AutoConfiguration.java
+++ springboot-starter/src/main/java/com/codingapi/springboot/framework/AutoConfiguration.java
@@ -1,8 +1,21 @@
 package com.codingapi.springboot.framework;
 
+import com.codingapi.springboot.framework.utils.VersionUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AutoConfiguration {
+public class AutoConfiguration implements InitializingBean {
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        String version = VersionUtils.getDriverVersion();
+        this.printBanner(version);
+    }
+
+    public void printBanner(String version) {
+        System.out.println("------------------------------------------------------");
+        System.out.println("\t\tCodingApi SpringBoot-Starter " + version);
+        System.out.println("------------------------------------------------------");
+    }
 }
